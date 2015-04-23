0fe7d03ad8ebd1a880d74b317ae9855ed3d40e3c
erin <erin314@naver.com>
2015-04-23 20:43:03 +0900

    img format

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -1,5 +1,15 @@
 var socket = io();
+var elImgs = document.querySelectorAll('#imgs img');
+console.log(elImgs);
+var elImg = elImgs[0];
 
+var messagesStorage = ['나를 데리고 놀아줘요 뿌잉뿌잉',
+              '나에게 입을 맞추다니! 난 사랑받는 곰이네요',
+              '머리 쓰담쓰담 해줘서 기분이 좋아졌어요',
+              '나랑 손잡고 밖에 놀러가요~~',
+              '수업들으러 같이 갈래요?',
+              '나 발냄새 난다구요 만자지 마요',
+              '아이쿠 간지러워요']
 
 $('form').submit(function() {
     socket.emit('chat message', $('#m').val());
@@ -7,12 +17,22 @@ $('form').submit(function() {
     return false;
 });
 
-
-socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
+socket.on('chat message', function(num) {
+    getMsg(num);
+    getImg(num);
 });
 
+function getMsg(num) {
+  var msg = messagesStorage[num];
+  $('#message').html(msg);
+}
+function getImg (num) {
+  elImg.classList.remove('action');
+  elImg = elImgs[num];
+  elImg.classList.add('action');
+}
 
-socket.on('button', function(msg) {
-    $('#messages').append($('<li>').text(msg));
-})
+window.addEventListener("load", function() {
+  getMsg(0);
+  getImg(0);
+});
--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,4 @@
 module.exports = function(http) {
-    //1 입
-    //2 머리
-    //3 손1
-    //4 손2
-    //5 발1
-    //6 발2
 
     var SerialPort = require("serialport")
     var serialPort = new SerialPort.SerialPort("/dev/tty.usbserial-A601LQZY", {
@@ -26,27 +20,18 @@ module.exports = function(http) {
         });
 
         serialPort.on('data', function(data) {
-            console.log('data received: ' + data);
-            io.emit('chat message', data);
-            
-            switch (data) {
-                case '1':
-                    io.emit('chat message', '나에게 입을 맞추다니! 난 사랑받는 곰이네요');
-                    break;
-                case '2':
-                    io.emit('chat message', '머리 쓰담쓰담 해줘서 기분이 좋아졌어요');
-                    break;
-                case '3':
-                    io.emit('chat message', '나랑 손잡고 밖에 놀러가요~~');
-                    break;
-                case '4':
-                    io.emit('chat message', '수업들으러 같이 갈래요?');
-                    break;
-                case '5':
-                    io.emit('chat message', '나 발냄새 난다구요 만자지 마요');
-                    break;
-                case '6':
-                    io.emit('chat message', '아이쿠 간지러워요');
+            var num = data*1;
+            console.log('data received: ' +num);
+            // io.emit('chat message', num);
+
+            switch (num) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    io.emit('chat message', num);
                     break;
                 default:
             }
